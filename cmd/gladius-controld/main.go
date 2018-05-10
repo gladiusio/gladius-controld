f39e8a9099b54c305abe2733d60874d5e4cafeee
Nathan Feldsien <nate@studioember.com>
2018-05-10 16:33:18 -0400

    Cleanup

--- cmd/gladius-controld/main.go
+++ cmd/gladius-controld/main.go
@@ -18,23 +18,3 @@ func main() {
 	// Run the function "run" in newtworkd as a service
 	manager.RunService(name, displayName, description, routing.Start)
 }
-
-//
-// import (
-// 	"github.com/gladiusio/gladius-utils/config"
-// 	"github.com/gladiusio/gladius-utils/init/manager"
-// )
-//
-// // Main entry-point for the service
-// func main() {
-// 	// Setup config handling
-// 	config.SetupConfig("gladius-controld", config.NetworkDaemonDefaults())
-// 	// Define some variables
-// 	name, displayName, description :=
-// 		"GladiusControlDaemon",
-// 		"Gladius Control Daemon",
-// 		"Gladius Control Daemon"
-//
-// 	// Run the function "run" in newtworkd as a service
-// 	manager.RunService(name, displayName, description, controld.Run)
-// }
