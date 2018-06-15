0a385aa93620458921675bde8e6de8d8d3f1da3b
Alex Godwin <alexkgodwin@gmail.com>
2018-06-15 17:37:15 -0400

    Started p2p logging implementation

--- pkg/p2p/peer/rpc.go
+++ pkg/p2p/peer/rpc.go
@@ -8,6 +8,10 @@ type RPCState struct {
 	p Peer
 }
 
+type RPCLogging struct {
+	p Peer
+}
+
 func (s *RPCState) Update(arg *signature.SignedMessage, reply *string) error {
 	if arg.IsVerified() {
 		s.p.UpdateAndPushState(arg)
@@ -22,4 +26,11 @@ func (s *RPCState) Get(args int, reply *string) error {
 	jsonstring, err := s.p.GetState().GetJSON()
 	*reply = string(jsonstring)
 	return err
+}
+
+func (l *RPCLogging) PassMessage(arg *signature.SignedMessage, reply *string) {
+	if arg.IsVerified() {
+		// TODO: Pass message and don't log it on this machine unless a logging flag is specified in the config
+		*reply = "Passed Message"
+	}
 }
