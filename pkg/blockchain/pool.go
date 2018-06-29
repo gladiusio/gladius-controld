5072254e1da22fba755ea84e3bc445552c05fbc6
Alex Godwin <alexkgodwin@gmail.com>
2018-06-29 14:31:48 -0400

    Removed error printing

--- pkg/blockchain/pool.go
+++ pkg/blockchain/pool.go
@@ -3,7 +3,6 @@ package blockchain
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"strings"
 	"sync"
@@ -131,7 +130,6 @@ func PoolNodesWithData(poolAddress common.Address, nodeAddresses *[]common.Addre
 				defer wg.Done()
 				nodeApplication, err1 := NodeRetrieveApplication(&nodeAddress, &poolAddress)
 				if err1 != nil {
-					fmt.Println(err1)
 					err = err1
 					running = false
 					return
