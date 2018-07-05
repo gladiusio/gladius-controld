1c52ad77362487d415cd720cf7f0f69e82577e5b
Alex Godwin <alexkgodwin@gmail.com>
2018-07-05 13:07:36 -0400

    Vagrant additions

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,18 @@
 Vagrant.configure("2") do |config|
+  config.vm.define "node_seed" do |seed|
+    seed.vm.box = "ubuntu/trusty64"
+    # Setup networking
+    seed.vm.network "private_network", ip: "172.28.128.2"
+    seed.vm.synced_folder "./", "/gladius/"
+
+    seed.trigger.after :up do |trigger|
+      trigger.run_remote = {inline: "/gladius/test_setup_p2p.sh"}
+    end
+  end
   $i = 0
   $num_of_vms = 10
   while $i < $num_of_vms do
     config.vm.define "node_#$i" do |node|
-       # Use alpine so we can make lots and lots of VMs
        node.vm.box = "ubuntu/trusty64"
        # Setup networking
        node.vm.network "private_network", type: "dhcp"
@@ -12,7 +21,7 @@ Vagrant.configure("2") do |config|
        node.trigger.after :up do |trigger|
          trigger.run_remote = {inline: "/gladius/test_setup_p2p.sh"}
        end
-   end
+    end
    $i +=1
   end
 end
--- test_setup_p2p.sh
+++ test_setup_p2p.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 
 # Run the conrold
-./build/gladius-controld
+/gladius/build/gladius-controld install
+/gladius/build/gladius-controld start
 
 # Wait for it to start
-sleep 10s
+sleep 5s
 
 # Create a new gladius account
+body='{"passphrase":"password"}'
+
+curl -X POST "http://localhost:3001/api/keystore/wallet/create" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d "$body"
+
+sleep 1s
 
 # Fetch the discovery node state
+body='{"ip":"192.168.10.2.", "passphrase":"password"}'
+
+curl -X POST "http://localhost:3001/api//state/pull" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d "$body"
+
+sleep 1s
 
 # Push a message with the machine IP in it
+body='{"ip":"172.28.128.2", "passphrase":"password"}'
+
+curl -X POST "http://localhost:3001/api//state/pull" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d "$body"
