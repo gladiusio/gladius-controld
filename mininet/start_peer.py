34c3ca85b4748ef82b7d8a0fd6a40b86a8f8b169
Alex Godwin <alexkgodwin@gmail.com>
2018-07-12 15:39:33 -0400

    Peer fixes

--- mininet/start_peer.py
+++ mininet/start_peer.py
@@ -10,13 +10,10 @@ def start_peer(node_name, discovery_ip):
     # Sign the intorduction message
     url = "http://localhost:3001/api/p2p/message/sign"
     # Get our local IP address
-
     s = subprocess.check_output(
         "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1 | tail -n 2", shell=True).rstrip()
     data = '''{"message": {"node": {"ip_address": "''' + \
         s + '''"}}, "passphrase": "password"}'''
-
-    print data
     singed_message = requests.post(url, data=data)
     singed_message_string = json.dumps(singed_message.json()['response'])
 
@@ -34,8 +31,6 @@ def start_peer(node_name, discovery_ip):
     data = singed_message_string
     response = requests.post(url, data=data).text
     print "push: " + response
-
-
 
 if __name__ == "__main__":
     start_peer(sys.argv[1], sys.argv[2])
