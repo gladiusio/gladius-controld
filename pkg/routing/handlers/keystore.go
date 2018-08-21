ebf917e7650834662943e5aad459f5b49b85777e
Nathan Grey <nate@studioember.com>
2018-08-21 13:54:29 -0400

    Fixes status error on account open endpoint

--- pkg/routing/handlers/keystore.go
+++ pkg/routing/handlers/keystore.go
@@ -75,6 +75,14 @@ func KeystoreAccountUnlockHandler(ga *blockchain.GladiusAccountManager) func(w h
 			return
 		}
 
+		address, err := ga.GetAccountAddress()
+		addressResponse := response.AddressResponse{Address: *address}
+
+		if ga.Unlocked() {
+			ResponseHandler(w, r, "Account is already unlocked", true, nil, addressResponse, nil)
+			return
+		}
+
 		accountBody, err := passphraseDecoder(w, r)
 		if err != nil {
 			ErrorHandler(w, r, "Could not find `passphrase` in request", err, http.StatusBadRequest)
@@ -82,18 +90,21 @@ func KeystoreAccountUnlockHandler(ga *blockchain.GladiusAccountManager) func(w h
 		}
 
 		success, err := ga.UnlockAccount(accountBody.Passphrase)
-		if success == false || err != nil {
-			ErrorHandler(w, r, "Wallet could not be opened, passphrase is incorrect", err, http.StatusMethodNotAllowed)
+		if !success || err != nil {
+			accountErr := AccountErrorHandler(w, r, ga)
+			if accountErr != nil {
+				return
+			}
+
+			ErrorHandler(w, r, "Wallet could not be opened", err, http.StatusInternalServerError)
 			return
 		}
 
-		address, err := ga.GetAccountAddress()
 		if err != nil {
 			ErrorHandler(w, r, "Account address could not be retrieved", err, http.StatusInternalServerError)
 			return
 		}
 
-		addressResponse := response.AddressResponse{Address: *address}
 
 		ResponseHandler(w, r, "null", true, nil, addressResponse, nil)
 	}
